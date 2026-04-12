0c9695e58bb11db357a79801a2922ac41d3de799
Alexandr Inweb <drafter.myself@gmail.com>
2026-04-13 01:01:50 +0300

    initial repository. structure, pages, base integration with wp cms

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'; // Импортируем адаптер для Node.js
+
+// https://astro.build/config
+export default defineConfig({
+  // Устанавливаем режим вывода на серверный (SSR)
+  output: 'server',
+
+  // Настраиваем адаптер для работы на VPS
+  adapter: node({
+    mode: 'standalone', // Проект будет запускаться как самостоятельное Node-приложение
+  }),
+
+	trailingSlash: 'never', // Указываем Astro никогда не добавлять слэш в конце URL
+
+  // Настройки Vite для локальной разработки
+  vite: {
+    server: {
+      // Разрешаем работу через наш локальный домен
+      allowedHosts: ['motopuzzle.local', 'api-motopuzzle.local'],
+    },
+    // Опционально: если столкнешься с проблемами кэширования в браузере
+    optimizeDeps: {
+      exclude: ['@astrojs/node'],
+    },
+  },
+
+  // Если в будущем планируешь использовать изображения через Astro.Image
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'api-motopuzzle.local',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api-motopuzzle.local',
+      },
+    ],
+  },
+});
